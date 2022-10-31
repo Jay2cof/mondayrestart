83eeaa56c4d7068bc823846cd69df5d0b7419dad
Justice Onuoha <justice101onuoha@gmail.com>
2022-10-31 12:47:05 +0100

    new file

--- restart/job/restart1.py
+++ restart/job/restart1.py
@@ -6,3 +6,15 @@ app = FastAPI()
 @app.get("/")
 def family():
     return {"father": "son"}
+
+app.get("/job")
+def getJobs(): 
+    return [{
+        "id": "uid1",
+        "title": "Nice job",
+        "description": "super nice job"
+    },{
+        "id": "uid2",
+        "title": "Best Job",
+        "description": "super working environment"
+    }]
