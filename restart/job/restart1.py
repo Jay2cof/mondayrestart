2ad43a69df2601fc42d864d0b67a19c05a23bf78
Justice Onuoha <justice101onuoha@gmail.com>
2022-11-01 09:24:44 +0100

    new changes

--- restart/job/restart1.py
+++ restart/job/restart1.py
@@ -1,21 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+
+Jobs_data= [{
+        "id": "uid1",
+        "title": "Nice job1",
+        "description": "super nice job"
+}, {
+        "id": "uid2",
+        "title": "Nice Job2",
+        "description": "super nice job2"
+}]
 
 
 @app.get("/")
 def family():
     return {"father": "son"}
 
+
 @app.get("/job")
 def getJobs(): 
-    return [{
-        "id": "uid1",
-        "title": "Nice job",
-        "description": "super nice job"
-    },{
-        "id": "uid2",
-        "title": "Best Job",
-        "description": "super working environment"
-    }]
+    return Jobs_data
+
+
+@app.get("/job/{id}")
+def getJobs(id): 
+    for job in Jobs_data:
+        if (job["id"] == id):
+            return job
+    raise HTTPException(
+        status_code=404, detail="Job with id " + id + " not found")
+
+
+    
 
